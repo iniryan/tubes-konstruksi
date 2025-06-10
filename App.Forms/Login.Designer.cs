77250f4175bb773a010cd4af54c13c6060eaf18d
fidiramadhina <9hwahyufidi23@gmail.com>
2025-06-10 14:36:08 +0700

    Membuat form login, register

--- App.Forms/Login.Designer.cs
+++ App.Forms/Login.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            loginButton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            registerButton = new Button();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(269, 163);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(270, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(269, 221);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(270, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(359, 269);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(94, 29);
+            loginButton.TabIndex = 2;
+            loginButton.Text = "Login";
+            loginButton.UseVisualStyleBackColor = true;
+            loginButton.Click += loginButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(269, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(269, 198);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(527, 21);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Belum punya akun?";
+            // 
+            // registerButton
+            // 
+            registerButton.Location = new Point(670, 17);
+            registerButton.Name = "registerButton";
+            registerButton.Size = new Size(94, 29);
+            registerButton.TabIndex = 6;
+            registerButton.Text = "Register";
+            registerButton.UseVisualStyleBackColor = true;
+            registerButton.Click += registerButton_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 21);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Halaman Login";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(registerButton);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(loginButton);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Login";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button loginButton;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button registerButton;
+        private Label label4;
     }
 }
--- App.Forms/Register.cs
+++ App.Forms/Register.cs
@@ -16,5 +16,39 @@ namespace App.Forms
         {
             InitializeComponent();
         }
+
+        private void daftarButton_Click(object sender, EventArgs e)
+        {
+            string nama = namaLengkap.Text;
+            string alamat = alamatLengkap.Text;
+            string noHP = noHandphone.Text;
+            string username = newUsername.Text;
+            string password = newPassword.Text;
+
+            if (string.IsNullOrWhiteSpace(nama) || string.IsNullOrWhiteSpace(alamat) ||
+                string.IsNullOrWhiteSpace(noHP) || string.IsNullOrWhiteSpace(username) ||
+                string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Semua field harus diisi.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            bool isSuccess = AuthService.Register(username, password);
+
+            if (isSuccess)
+            {
+                MessageBox.Show("Registrasi berhasil! Silakan login.", "Registrasi Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Username sudah terdaftar. Silakan gunakan username lain.", "Registration Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
